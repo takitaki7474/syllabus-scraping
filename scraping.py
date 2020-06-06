7e230888186e9af06703be437e8e32aba03ee9f9
oota <shark747474@gmail.com>
2020-06-06 22:45:15 +0900

    add click_elem_id

--- scraping.py
+++ scraping.py
@@ -10,3 +10,7 @@ def switch_to_frame(driver, frame_name):
     driver.switch_to.default_content()
     WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.NAME, frame_name)))
     driver.switch_to.frame(driver.find_element_by_name(frame_name))
+
+def click_elem_id(driver, elem_id):
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, elem_id)))
+    driver.find_element_by_id(elem_id).click()
