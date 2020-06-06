853a0364dc139357c80ff41523e14cd5fc714a6d
oota <shark747474@gmail.com>
2020-06-07 04:33:17 +0900

    add generate_target_category_syllabus

--- scraping.py
+++ scraping.py
@@ -38,7 +38,23 @@ def generate_id_list_and_degree_id(soup):
 
 def generate_target_category_syllabus(soup):
     target_category_syllabus = []
-    
+    one_subject_syllabus = {}
+    tr_tag_list = soup.find_all("tr")
+    for tr_tag in tr_tag_list:
+        exists_td_tag = (tr_tag.find_all("td", {"class": "list-odd-left"}) != [])
+        if not exists_td_tag:
+            continue
+        td_tag_list = tr_tag.find_all("td", {"class": "list-odd-left"})
+        one_subject_syllabus["category"] = td_tag_list[0].get_text()
+        one_subject_syllabus["semester"] = td_tag_list[1].get_text()
+        one_subject_syllabus["subject_name"] = td_tag_list[2].get_text().replace("\n", "")
+        one_subject_syllabus["class_schedule"] = td_tag_list[3].get_text(",").split(",")
+        one_subject_syllabus["classroom"] = td_tag_list[4].get_text(",").split(",")
+        one_subject_syllabus["teacher"] = td_tag_list[5].get_text().replace("\u3000", " ")
+        target_category_syllabus.append(one_subject_syllabus)
+        one_subject_syllabus = {}
+    print("読込済:  {0}".format(target_category_syllabus[0]["category"]))
+    return target_category_syllabus
 
 def scraping_syllabus(url):
     driver = webdriver.Chrome()
@@ -61,10 +77,13 @@ def scraping_syllabus(url):
         switch_to_frame(driver, "public_main")
         time.sleep(5)
         soup = generate_current_page_html(driver)
-        is_table_tag = (soup.find_all("table", {"class": "list"}) != [])
-        if is_table_tag:
+        exists_table_tag = (soup.find_all("table", {"class": "list"}) != [])
+        if exists_table_tag:
             if target_syllabus == "Bachelor":
                 bachelor_syllabus += generate_target_category_syllabus(soup)
             elif target_syllabus == "Master_and_Doctor":
                 master_and_doctor_syllabus += generate_target_category_syllabus(soup)
         switch_to_frame(driver, "frame2")
+    syllabus["Bachelor"] = bachelor_syllabus
+    syllabus["Master_and_Doctor"] = master_and_doctor_syllabus
+    
