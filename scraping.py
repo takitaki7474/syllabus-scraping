3ee9f83800408d334d01024f9c4cd17daf76d59d
oota <shark747474@gmail.com>
2020-06-06 22:40:19 +0900

    update switch_to_frame

--- scraping.py
+++ scraping.py
@@ -7,5 +7,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 def switch_to_frame(driver, frame_name):
+    driver.switch_to.default_content()
     WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.NAME, frame_name)))
     driver.switch_to.frame(driver.find_element_by_name(frame_name))
