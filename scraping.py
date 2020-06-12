b3f0f2d01a3e2c450f032227ba949c225aa4846a
oota <shark747474@gmail.com>
2020-06-13 02:37:29 +0900

    update scraping

--- scraping.py
+++ scraping.py
@@ -1,3 +1,5 @@
+from common import operate_html as op
+from common import save_dict as sd
 
 def note_degree_id(tag_list):
     degree_id = {}
@@ -29,3 +31,37 @@ def make_current_page_syllabus(soup, page_count):
     if len(current_page_syllabus) != 0:
         print("読込済:  {0}  {1}p".format(current_page_syllabus[0]["category"], page_count))
     return current_page_syllabus
+
+def make_all_page_syllabus(driver, menu_ids, degree_id):
+    syllabus = {} # 全学科のシラバス
+    bachelor_syllabus = [] # 学部のシラバス
+    master_and_doctor_syllabus = [] # 大学院のシラバス
+    target_syllabus = "Bachelor" # 生成対象のシラバス
+
+    for menu_id in menu_ids:
+        if menu_id == degree_id["Bachelor"]:
+            target_syllabus = "Bachelor" # 学部のシラバスが対象
+        elif menu_id == degree_id["Master_and_Doctor"]:
+            target_syllabus = "Master_and_Doctor" # 大学院のシラバスが対象
+        op.click_elem_by_id(driver, menu_id)
+        op.switch_to_frame(driver, "public_main")
+        page_count = 1
+        while(1):
+            time.sleep(5)
+            soup = op.load_html(driver)
+            exists_table = (soup.find_all("table", {"class": "list"}) != [])
+            exists_next_page = (soup.find_all("a", {"title": "next page"}) != [])
+            if exists_table:
+                if target_syllabus == "Bachelor":
+                    bachelor_syllabus += make_current_page_syllabus(soup, page_count)
+                elif target_syllabus == "Master_and_Doctor":
+                    master_and_doctor_syllabus += make_current_page_syllabus(soup, page_count)
+            if exists_next_page:
+                page_count += 1
+                op.click_elem_by_xpath(driver, "//a[@title='next page']")
+            else:
+                break
+        op.switch_to_frame(driver, "frame2")
+    syllabus["Bachelor"] = bachelor_syllabus
+    syllabus["Master_and_Doctor"] = master_and_doctor_syllabus
+    return syllabus
