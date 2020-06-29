45ef21fe09a16fed547e1a51fa5a96bb3d938968
oota <shark747474@gmail.com>
2020-06-29 21:19:30 +0900

    Add reformat_syllabus function

--- reformat_syllabus.py
+++ reformat_syllabus.py
@@ -1,5 +1,5 @@
 import json
-import jaconv
+import jaconv #全角半角変換
 from common import save_dict as sd
 
 def load_syllabus(filepath):
@@ -7,11 +7,7 @@ def load_syllabus(filepath):
         syllabus = json.load(f)
     return syllabus
 
-if __name__=="__main__":
-    loadpath = "./saved_json/syllabus.json"
-    savepath = "./saved_json/reformatted_syllabus.json"
-
-    syllabus = load_syllabus(loadpath)
+def reformat_syllabus(syllabus):
     reformatted_syllabus = []
     reformatted_subject = {}
     id_count = 0
@@ -34,3 +30,30 @@ if __name__=="__main__":
             print("error")
             break
         reformatted_subject = {}
+    # Master_and_Doctorの処理
+    for subject in syllabus["Master_and_Doctor"]:
+        reformatted_subject["category"] = jaconv.z2h(subject["category"] ,digit=True, ascii=True)
+        reformatted_subject["semester"] = jaconv.z2h(subject["semester"] ,digit=True, ascii=True)
+        reformatted_subject["subjectName"] = jaconv.z2h(subject["subject_name"] ,digit=True, ascii=True)
+        reformatted_subject["teacher"] = jaconv.z2h(subject["teacher"] ,digit=True, ascii=True)
+        reformatted_subject["degree"] = "graduate"
+        if len(subject["class_schedule"]) == len(subject["classroom"]):
+            for i in range(len(subject["class_schedule"])):
+                class_schedule = jaconv.z2h(subject["class_schedule"][i] ,digit=True, ascii=True)
+                reformatted_subject["schedule"] = class_schedule.replace(reformatted_subject["semester"], "")
+                reformatted_subject["classroom"] = jaconv.z2h(subject["classroom"][i] ,digit=True, ascii=True)
+                reformatted_subject["id"] = id_count
+                reformatted_syllabus.append(reformatted_subject.copy())
+                id_count += 1
+        else:
+            print("error")
+            break
+        reformatted_subject = {}
+    return reformatted_syllabus
+
+if __name__=="__main__":
+    loadpath = "./saved_json/syllabus.json"
+    savepath = "./saved_json/reformatted_syllabus.json"
+
+    syllabus = load_syllabus(loadpath)
+    reformatted_syllabus = reformat_syllabus(syllabus)
